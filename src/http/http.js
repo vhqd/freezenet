2d013e7ecbde25a810f598dd91f06718725fd089
vhqd <www.zhangqiangwo@qq.com>
2018-08-10 19:54:20 +0800

    a little change of http.js

--- src/http/http.js
+++ src/http/http.js
@@ -5,6 +5,7 @@ const host = store.state.host;
 /**
  * 获取首页轮播图
  * @param limit分页条数
+ * @param page当前页码
  */
 export const getIndexBanner = (limit,page) =>{ return axios.get(host + "/api/banner?limit=" + limit + "&page=" + page).then((res) => {return res;}).catch((error) => {return error.response})};
 /**
